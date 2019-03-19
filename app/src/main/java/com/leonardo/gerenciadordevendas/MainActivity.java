d90cb41b5eed0f8f84e4901cb8a0b77737eeaefa
PsDaniel <danieljfsantos@gmail.com>
2019-03-19 10:55:23 -0300

    AlerdDialog ao sair do app

--- app/src/main/java/com/leonardo/gerenciadordevendas/MainActivity.java
+++ app/src/main/java/com/leonardo/gerenciadordevendas/MainActivity.java
@@ -1,10 +1,13 @@
 package com.leonardo.gerenciadordevendas;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int TELA_LISTA_CLIENTES = 2;
 
     Button btn_cadastrar_cliente,
-            btn_produtos,btn_vendas,
+            btn_produtos, btn_vendas,
             btn_sair_app, btn_lista_de_clientes;
 
     @Override
@@ -38,12 +41,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btn_sair_app.setOnClickListener(new View.OnClickListener() {
+    }
+
+    public void sairApp(View view) {
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Sair");
+        dialog.setMessage("Deseja sair do app?");
+
+        //configura o cancelamento.
+        dialog.setCancelable(false);
+        //icone
+        dialog.setIcon(android.R.drawable.ic_dialog_info);
+
+        //Ações
+        dialog.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(DialogInterface dialog, int which) {
                 finish();
             }
         });
+
+        dialog.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        //criar e exibir
+        dialog.create();
+        dialog.show();
     }
 
     private void binding() {
